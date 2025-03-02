0a9378b1174f998b5bdb2902e6053f87a58a35ac
jon <zjon234@gmail.cm>
2025-03-02 12:33:01 +0800

    first commit

---
+++ src/slog.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+
+write_log(){
+
+	if [ ! -f ${2}.log ];
+	then
+		touch $2.log
+	fi
+
+	modify_date=$(stat -f "%Sm" -t "%Y%m%d" "$2.log")
+
+	current_date=$(date "+%Y%m%d")
+
+	timers=$(date "+%Y-%m-%d %H:%M:%S")
+
+	if [ "${modify_date}" != "${current_date}" ];
+	then
+		mv $2.log ${2}${modify_date}.log
+	fi
+
+	echo  "[${timers}] ${1}" >> $2.log
+
+}
+
+log_msg=$1
+
+
+write_log "$log_msg" $HOME/logs/appLog
