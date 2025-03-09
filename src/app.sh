38b683484845d22c37bae8c5c579ceefd4e464c2
jon <zjon234@gmail.cm>
2025-03-09 19:52:33 +0800

    first commit

---
+++ src/app.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+export JAVA_OPTS="-Xms512m -Xmx2024m -Xss1m -XX:+UseParallelGC -Xloggc:./logs/gc.log -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintGCTimeStamps"
+
+export APP_CONF="./config/application.yml,./config/application-gauss.yml"
+
+export JAVA_HOME="xxxxxxx"
+
+simpler_command=$1
+
+if [ -z "$2" ];
+then
+	app_name="compare-app.jar"
+else
+	app_name=$2
+fi
+
+start(){
+
+    nohup $JAVA_HOME $JAVA_OPTS -jar $app_name  --spring.config.location=$APP_CONF  >> ./logs/app.log 2>&1 &
+    local process_ids=$(pgrep -f $app_name)
+
+   if [ -z "$process_ids" ]; then
+        echo "-------------- start app failed ---------------------"
+    else
+        echo "-------------- start app success --------------------"
+    fi
+}
+
+stop(){
+    local process_ids=$(pgrep -f $app_name)
+    if [ -z "$process_ids" ]; then
+        echo "no matching process was found"
+    else
+        echo "$process_ids" | tee /dev/stderr | xargs -I {} kill -9 {}
+        echo "-------------- kill app success  --------------------"
+    fi
+
+}
+
+main(){
+
+    if [ ! -d "./logs" ];
+    then
+        echo "creating logs directory"
+        mkdir -p ./logs
+    fi
+
+    case $simpler_command in
+      start)
+            local process_ids=$(pgrep -f $app_name)
+            if [ ! -z "$process_ids" ]; then
+                echo "the process exists: $process_ids"
+                exit 0
+            fi
+
+            start
+        ;;
+      stop)
+            stop
+        ;;
+      *)
+            echo "please enter the start or stop command"
+        ;;
+    esac
+}
+
+
+main
---
+++ src/try_out.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Hello, World!"
+
+file_path=$1
+
+while fis= read -r line
+do
+    echo "$line"
+
+done < $file_path
